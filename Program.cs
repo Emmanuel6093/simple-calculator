c2c9e480ffad334a1e683c31671d1a2c7c462ef0
Emmanuel6093 <elorenzo6093@gmail.com>
2024-05-27 14:07:27 -0400

    setting up for user input

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace CalculatorApp
     {
         static void Main(string[] args)
         {
-            
+            bool continueCalculation = true;
+
+            while (continueCalculation) 
+            {
+                Console.Clear();
+                Console.WriteLine("Console Calculator in C#\r");
+                Console.WriteLine("------------------------\n");
+                
+                Console.WriteLine("Type a number, and then press Enter:");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                
+                Console.WriteLine("Type another number, and then press Enter:");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+            }
         }
     }
 }
